5ddd095abab9856f4a20b73146ec5b7cba8bf994
乌日巴图 <wuribatu@wuribatudemacbook-pro.local>
2016-01-03 15:49:16 +0800

    Initial Commit

---
+++ BTDrawTheGeometry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BTDrawTheGeometry
+//
+//  Created by 乌日巴图 on 16/1/3.
+//  Copyright © 2016年 wuribatu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BTDrawTheGeometryTests/BTDrawTheGeometryTests.m
@@ -0,0 +1,39 @@
+//
+//  BTDrawTheGeometryTests.m
+//  BTDrawTheGeometryTests
+//
+//  Created by 乌日巴图 on 16/1/3.
+//  Copyright © 2016年 wuribatu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BTDrawTheGeometryTests : XCTestCase
+
+@end
+
+@implementation BTDrawTheGeometryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
